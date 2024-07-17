b03524e54eb5a4885d6c03580629fa4ccafbe291
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-07-17 09:22:05 +0700

    upload file thanh cong

--- convex/files.ts
+++ convex/files.ts
@@ -3,6 +3,15 @@ import { ConvexError, v } from 'convex/values'
 import {mutation, MutationCtx, query, QueryCtx} from './_generated/server'
 import { getUser } from './users';
 
+export const generateUploadUrl = mutation(async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if(!identity){
+        throw new ConvexError('you must be logged in to upload a file');
+    }
+    return await ctx.storage.generateUploadUrl()
+
+});
+
 
 async function hasAccessToOrg(ctx:QueryCtx|MutationCtx,tokenIdentifier:string,orgId:string
     ){
@@ -16,10 +25,12 @@ async function hasAccessToOrg(ctx:QueryCtx|MutationCtx,tokenIdentifier:string,or
 export const createFile = mutation({
     args:{
         name:v.string(),
+        fileId:v.id("_storage"),
         orgId:v.string(),
     },
     async handler(ctx,args)
     {
+    
         const identity = await ctx.auth.getUserIdentity();
         console.log(identity);
         if(!identity){
@@ -33,6 +44,7 @@ export const createFile = mutation({
                 await ctx.db.insert("files",{
             name:args.name,
             orgId:args.orgId,
+            fileId: args.fileId,
         });
     },
 });
