036c4eedfacc8cbb10b17923b5d042715628266e
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-08-10 16:23:39 +0700

    Update email-template.tsx

--- src/app/dashboard/notifications/email-template.tsx
+++ src/app/dashboard/notifications/email-template.tsx
@@ -4,6 +4,14 @@ interface EmailTemplateProps {
   message: string;
 }
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = () => {
-    return <></>
-}
+export const EmailTemplate: React.FC<EmailTemplateProps> = ({ message }) => {
+  return (
+    <div>
+      <h1>Thông Báo Mới</h1>
+      <p>{message}</p>
+      <p>Trân trọng,</p>
+      <p>Đội ngũ hỗ trợ Gmail 
+      </p>
+    </div>
+  );
+};
